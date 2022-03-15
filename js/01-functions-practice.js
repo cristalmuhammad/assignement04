875897be94fbe81915a4ca637631c9ae889052d4
Cristal Muhammad <cristalmuhammad@gmail.com>
2022-03-15 11:03:54 -0700

    initial commit

---
+++ js/01-functions-practice.js
@@ -0,0 +1,37 @@
+// STEP 1
+// function halfNumber(number) {
+// console.log(`Half of ${number} is ${number/2}`)
+// }
+//  halfNumber(5);
+
+//STEP 2
+// function squareNumber(number) {
+//     console.log(`The result of squaring the number ${number} is ${number * number}`)
+// }
+// squareNumber(3);
+
+//STEP 3
+// function percentOf(num1, num2) {
+//     console.log(`${num1} is  ${num1/num2 * 100}% of ${num2}`);
+// }
+// percentOf(20, 100);
+
+//STEP 4
+
+// function findModulus(num1, num2){
+//    console.log(`${num2%num1} is the modulus of ${num1} & ${num2} `);
+// }
+// findModulus(7,38 )
+
+//STEP 5
+// This returns Hello Cristal Muhammad
+// function greet(name, lastName) { 
+//     console.log('Hello '  + name + ' ' + lastName);
+//     }
+//     greet('Cristal', 'Muhammad') 
+
+// function showYear() {
+//     const today = new Date();
+//     alert ( 'The year is' + today.getFullYear() );
+// }
+// showYear();
---
+++ js/02-rock-paper-scissors.js
@@ -0,0 +1,14 @@
+// let userChoice = prompt('Choose either, rock, paper, or scissors')
+let compChoice = Math.round((Math.random() *3) +1); {
+    console.log ('number')
+}
+// this should generate numbers btwn 1 and 3
+// if (compChoice = 1 ) {
+//     if (userChoice === 'rock')
+//     alert('Winner rock')
+// } else if (compChoice = 2) {
+//     if (userChoice === 'paper')
+//     alert ('loser paper ')
+// } else {
+//     alert ('scissors')
+// }
